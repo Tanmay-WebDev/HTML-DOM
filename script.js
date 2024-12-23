e01871b2545f81b2a79ab38fc688a42c01ad1e23
Tanmay-WebDev <umas81261@gmail.com>
2024-12-23 19:29:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function helloCoders(){
+    const coder = document.getElementById("coder");
+    coder.style.color = "purple";
+    coder.innerHTML = "You Have Clicked The Button";
+    coder.style.fontSize = "3em"
+    coder.style.textShadow = "1px 2px 5px purple"
+    coder.style.transition = "0.8s"
+}
